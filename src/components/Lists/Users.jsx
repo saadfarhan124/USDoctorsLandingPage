d84c2d53c738f3cac68dab4504acd661fe942f24
rsucapital <admin@usdoctors.co>
2021-02-09 20:37:58 +0500

    added sorting to users listing

--- src/components/Lists/Users.jsx
+++ src/components/Lists/Users.jsx
@@ -1,8 +1,26 @@
 import React  from "react";
-import { List, Datagrid, TextField, DateField, EmailField, ChipField, ShowButton } from 'react-admin';
+import { List, Datagrid, TextField, DateField, EmailField, ChipField, ShowButton, Filter, SelectInput } from 'react-admin';
+
+const UserFilter = (props) => (
+    <Filter {...props}>
+        <SelectInput source="Status" choices={[
+            { id: -1, name: 'Email Verification Pending' },
+            { id: 0, name: 'Pending' },
+            { id: 1, name: 'Active' },
+            { id: 2, name: 'Rejected' },
+            { id: 3, name: 'Deactivated' },
+            { id: 5, name: 'Leads' },
+        ]}/>
+
+        {/* <TextInput label="Search" source="q" alwaysOn /> */}
+        {/* <ReferenceInput label="User" source="user_id" reference="users" allowEmpty>
+        </ReferenceInput> */}
+    </Filter>
+);
+
 export const UserList = props => {
     return  (
-        <List {...props} bulkActionButtons={false}>
+        <List filters={<UserFilter />} {...props} bulkActionButtons={false}>
             <Datagrid>
                 <TextField label="Name" source="fullname" />
                 <DateField label="Registered At"  source="created_at" options={{ year: 'numeric', month: 'long', day: 'numeric' }}/>
